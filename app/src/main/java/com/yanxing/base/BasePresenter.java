fe5f42dae1165b576cba498fc9bff3d496bfface
lishuangxiang <lyxtime@163.com>
2016-12-22 16:49:58 +0800

    update

--- app/src/main/java/com/yanxing/base/BasePresenter.java
+++ app/src/main/java/com/yanxing/base/BasePresenter.java
@@ -8,7 +8,7 @@ public abstract class BasePresenter<T> {
     public T mView;
 
     /**
-     * 销毁持有的View
+     * 销毁持有的View，看到一种说法：说不是任何情况下Activity的onDestory方法都会被调用，故对View进行弱引用
      */
     public void onDestroy() {
         this.mView = null;
