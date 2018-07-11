a055a76e464342caaccc96a76cfab69b0d76605b
李双祥 <lyxtime@163.com>
2018-07-11 16:22:52 +0800

    优化

--- app/src/main/java/com/yanxing/view/MovieView.kt
+++ app/src/main/java/com/yanxing/view/MovieView.kt
@@ -9,7 +9,7 @@ import com.yanxing.model.Movie
 import com.yanxing.networklibrary.refresh.PullToRefresh
 
 /**
- *
+ * LifecycleProvider 用于取消RxJava订阅；PullToRefresh retrofit请求完成或出错，重置刷新组件状态
  * @author 李双祥 on 2018/7/5.
  */
 interface MovieView : BaseView ,OnLoadMoreListener, OnRefreshListener,PullToRefresh, LifecycleProvider<ActivityEvent> {
