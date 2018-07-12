0a2e716fe55c401fd47b38be108e8cbb475b492d
李双祥 <lyxtime@163.com>
2018-07-12 08:59:44 +0800

    优化

--- app/src/main/java/com/yanxing/view/MovieView.kt
+++ app/src/main/java/com/yanxing/view/MovieView.kt
@@ -2,17 +2,15 @@ package com.yanxing.view
 
 import com.aspsine.swipetoloadlayout.OnLoadMoreListener
 import com.aspsine.swipetoloadlayout.OnRefreshListener
-import com.trello.rxlifecycle2.LifecycleProvider
-import com.trello.rxlifecycle2.android.ActivityEvent
 import com.yanxing.base.BaseView
 import com.yanxing.model.Movie
 import com.yanxing.networklibrary.refresh.PullToRefresh
 
 /**
- * LifecycleProvider 用于取消RxJava订阅；PullToRefresh retrofit请求完成或出错，重置刷新组件状态
+ * PullToRefresh retrofit请求完成或出错，重置刷新组件状态
  * @author 李双祥 on 2018/7/5.
  */
-interface MovieView : BaseView ,OnLoadMoreListener, OnRefreshListener,PullToRefresh, LifecycleProvider<ActivityEvent> {
+interface MovieView : BaseView ,OnLoadMoreListener, OnRefreshListener,PullToRefresh{
 
     fun getMovieList(movie: Movie?)
 }
